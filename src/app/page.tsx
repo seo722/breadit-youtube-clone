cc16e5bd0a7a773f5af11bd7cb1c39b5430a27fb
seo722 <isogood11@naver.com>
2023-10-24 09:09:26 +0900

    13. custom feed for logged in & general feed for logged out users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import CustomFeed from '@/components/homepage/CustomFeed';
+import GeneralFeed from '@/components/homepage/GeneralFeed';
 import { buttonVariants } from '@/components/ui/Button';
 import { getAuthSession } from '@/lib/auth';
 import { cn } from '@/lib/utils';
@@ -14,9 +16,10 @@ export default async function Home() {
     <>
       <h1 className="font-bold text-3xl md:text-4xl">Your feed</h1>
       <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-6">
-        {/* {session ? <CustomFeed /> : <GeneralFeed />} */}
         {/* display either custom feed or general feed */}
-        feed
+        {/* @ts-expect-error server component */}
+        {session ? <CustomFeed /> : <GeneralFeed />}
+
         {/* subreddit info */}
         <div className="overflow-hidden h-fit rounded-lg border border-gray-200 order-first md:order-last">
           <div className="bg-emerald-100 px-6 py-4">
--- src/app/r/[slug]/layout.tsx
+++ src/app/r/[slug]/layout.tsx
@@ -52,6 +52,7 @@ const Layout = async ({ children, params: { slug } }: { children: React.ReactNod
     <div className="sm:container max-w-7xl mx-auto h-full pt-12">
       <div>
         {/* TODO: Button to take us back  */}
+
         <div className="grid grid-cols-1 md:grid-cols-3 gap-y-4 md:gap-x-4 py-7">
           <div className="flex flex-col col-span-2 space-y-6">{children}</div>
 
